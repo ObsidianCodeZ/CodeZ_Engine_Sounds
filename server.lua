d879e3ebae59678556a9a8696c2e59cb4ea75517
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-11 17:03:59 +0200

    Delete server.lua

--- server.lua
+++
@@ -1,6 +0,0 @@
-Citizen.CreateThread(function()
-    local resourceName = "^2 CodeZ_Engine_Sounds Started ("..GetCurrentResourceName()..")"
-    print("\n^1----------------------------------------------------------------------------------^7")
-    print(resourceName)
-    print("^1----------------------------------------------------------------------------------^7")
-end)
